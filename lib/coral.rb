ec86cb6d1626ba132dbcb65f75c54e7afeb66cad
Adrian Webb <adrian.webb@coralnexus.com>
2014-08-25 03:14:31 -0400

    Loader should only contain bare scaffolding to load CORL.

--- lib/coral.rb
+++ lib/coral.rb
@@ -1,27 +1,16 @@
 
-home = File.dirname(__FILE__)
-
-$:.unshift(home) unless
-  $:.include?(home) || $:.include?(File.expand_path(home))
-  
+#*******************************************************************************
+# CORL (Cluster Orchestration and Research Library)
+#
+# provides CORL (github.com/coralnexus/corl)
+# built on Nucleon (github.com/coralnexus/nucleon)
+#
+# A framework that provides a simple foundation for growing organically in
+# the cloud.
+#
+# Author:: Adrian Webb (mailto:adrian.webb@coralnexus.com)
+# License:: Apache license, version 2
 #-------------------------------------------------------------------------------
   
 require 'rubygems'
-require 'coral_cloud'
-require 'coral_plan'
-require 'coral_vagrant'
-
-#*******************************************************************************
-# Coral Meta Package
-#
-# This provides all Coral libraries in an easy to use package.
-#
-# Author::    Adrian Webb (mailto:adrian.webb@coraltech.net)
-# License::   GPLv3
-module Coral
-module Meta
-  
-  VERSION = File.read(File.join(File.dirname(__FILE__), '..', 'VERSION'))
-
-end
-end
+require 'corl'
